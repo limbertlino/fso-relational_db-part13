a74dff1b2dfe9ac1cf5999891cb685f651605e7e
Limbert Lino <limbertlino@gmail.com>
2024-09-23 17:31:39 -0400

    Done exercise 7

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
+require("express-async-errors");
 const app = express();
 
 const { PORT } = require("./utils/config");
 const { connectToDatabase } = require("./utils/db");
 const blogsRouter = require("./controllers/blogs");
 
+const errorHandler = (error, req, res, next) => {
+  if (error) {
+    return res
+      .status(400)
+      .json({ error: error.name, errorMessage: error.message });
+  }
+
+  next(error);
+};
+
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
+app.use(errorHandler);
 
 const start = async () => {
   await connectToDatabase();
