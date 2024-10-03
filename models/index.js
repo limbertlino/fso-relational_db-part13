0745e91b4903fbd7228f6625265884a265bd63bf
Limbert Lino <limbertlino@gmail.com>
2024-10-03 16:52:07 -0400

    Done exercise 19

--- models/index.js
+++ models/index.js
@@ -1,8 +1,12 @@
 const Blog = require("./Blog");
+const ReadingList = require("./reading_list");
 const User = require("./User");
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
+
+User.belongsToMany(Blog, { through: ReadingList, as: "reading_list_blogs" });
+Blog.belongsToMany(User, { through: ReadingList, as: "reading_list_users" });
 
 // Blog.sync({ alter: true });
 // User.sync({ alter: true });
